ba6aa63483fa3682a10401f8270ed81222497b4c
Pooya Parsa <pooya@pi0.ir>
2018-02-12 00:22:00 +0330

    example: add some meta

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,8 +1,19 @@
 const { resolve } = require('path')
 
+// Notice: This file is for example only.
+// For your own projects you may have to simply include @nuxtjs/python module
+// Please see official README and Docs
+
 module.exports = {
   rootDir: resolve(__dirname, '..'),
   srcDir: __dirname,
   dev: false,
-  modules: ['@@']
+  modules: [
+    '@@',
+    '@nuxtjs/pwa'
+  ],
+  manifest: {
+    name: 'Nuxt Python Module',
+    description: 'Write Nuxt.js apps in Python!'
+  }
 }
